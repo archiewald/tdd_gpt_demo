829bb00bd70e270088ca700394e270002e21b446
Artur Kozubek <artur.kozubek1@gmail.com>
2023-07-09 17:31:34 +0200

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+describe("Add Todo Feature", () => {
+  it.todo(
+    "should allow user to input a title for the new todo item, not exceeding 256 characters"
+  );
+  it.todo(
+    "should allow user to add an optional description to the new todo item, not exceeding 1024 characters"
+  );
+  it.todo("should allow user to save the new todo item");
+  it.todo(
+    "should display the new todo item on the 'Pending Todo' list upon successful saving"
+  );
+});
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Mark Todo as Done Feature", () => {
+  it.todo(
+    "should provide each todo item on the 'Pending Todo' list an option to 'Mark as Done'"
+  );
+  it.todo("should allow user to mark a todo item as done");
+  it.todo(
+    "should remove the todo item from 'Pending Todo' list and add it to 'Completed Todo' list upon successful marking"
+  );
+  it.todo(
+    "should display an appropriate error message if the marking process encounters an error"
+  );
+});
+
+describe("Delete Todo Feature", () => {
+  it.todo(
+    "should provide each todo item (in both 'Pending Todo' and 'Completed Todo' lists) an option to delete"
+  );
+  it.todo(
+    "should ask user for a confirmation before the deletion process is completed"
+  );
+  it.todo("should allow user to confirm and delete a todo item");
+  it.todo(
+    "should permanently remove the todo item from the list (whether 'Pending Todo' or 'Completed Todo') upon successful deletion"
+  );
+  it.todo(
+    "should display an appropriate error message if the deletion process encounters an error"
+  );
 });
