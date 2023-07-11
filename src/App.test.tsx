e1b093d156da2f6f0d42d081915b4aea371bed0a
Artur Kozubek <artur.kozubek1@gmail.com>
2023-07-11 09:57:09 +0200

    reset

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -18,6 +18,7 @@ describe("Add Todo Feature", () => {
     // then
     expect(input).toHaveValue("a".repeat(256));
   });
+
   it("should allow user to add an optional description to the new todo item, not exceeding 1024 characters", async () => {
     // given
     render(<App />);
@@ -47,90 +48,4 @@ describe("Add Todo Feature", () => {
     expect(titleInput).toHaveValue("");
     expect(descriptionInput).toHaveValue("");
   });
-
-  it("should display the new todo item on the 'Pending Todo' list upon successful saving", async () => {
-    // given
-    render(<App />);
-    const titleInput = screen.getByPlaceholderText("Todo Title");
-    const descriptionInput = screen.getByPlaceholderText("Todo Description");
-    const button = screen.getByRole("button", { name: /add todo/i });
-
-    // when
-    await userEvent.type(titleInput, "New Todo");
-    await userEvent.type(descriptionInput, "Todo description");
-    await userEvent.click(button);
-
-    // then
-    const pendingTodoList = screen.getByRole("list", { name: /pending todo/i });
-    const todoTitle = within(pendingTodoList).getByText("New Todo");
-    const todoDescription =
-      within(pendingTodoList).getByText("Todo description");
-
-    expect(todoTitle).toBeInTheDocument();
-    expect(todoDescription).toBeInTheDocument();
-  });
 });
-
-describe("Mark Todo as Done Feature", () => {
-  it("should allow user to mark a todo item as done", async () => {
-    // given
-    render(<App />);
-    await addTodo("New Todo", "Todo description");
-
-    // when
-    await markAsDone("New Todo");
-
-    // then
-    const completedTodoList = screen.getByRole("list", {
-      name: /completed todo/i,
-    });
-    const todoTitle = within(completedTodoList).getByText("New Todo");
-    expect(todoTitle).toBeInTheDocument();
-  });
-
-  it("should move multiple todo items to the 'Completed Todo' list upon marking them as done", async () => {
-    // given
-    render(<App />);
-    await addTodo("Eat breakfast");
-    await addTodo("Eat lunch");
-
-    // when
-    await markAsDone("Eat breakfast");
-    await markAsDone("Eat lunch");
-
-    // then
-    const completedTodoList = screen.getByRole("list", {
-      name: /completed todo/i,
-    });
-    const breakfastTodoTitle =
-      within(completedTodoList).getByText("Eat breakfast");
-    const lunchTodoTitle = within(completedTodoList).getByText("Eat lunch");
-
-    expect(breakfastTodoTitle).toBeInTheDocument();
-    expect(lunchTodoTitle).toBeInTheDocument();
-  });
-});
-
-const addTodo = async (title: string, description = " ") => {
-  const titleInput = screen.getByPlaceholderText("Todo Title");
-  const descriptionInput = screen.getByPlaceholderText("Todo Description");
-  const addButton = screen.getByRole("button", { name: /add todo/i });
-
-  await userEvent.type(titleInput, title);
-  await userEvent.type(descriptionInput, description);
-  await userEvent.click(addButton);
-};
-
-const markAsDone = async (title: string) => {
-  const pendingTodoList = screen.getByRole("list", { name: /pending todo/i });
-  const markAsDoneButtons = within(pendingTodoList).getAllByRole("button", {
-    name: /mark as done/i,
-  });
-  const todoItems = within(pendingTodoList).getAllByText(title);
-
-  const todoIndex = todoItems.findIndex((item) => item.textContent === title);
-
-  if (todoIndex !== -1) {
-    await userEvent.click(markAsDoneButtons[todoIndex]);
-  }
-};
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React, { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import "./App.css";
 
+type FormFields = {
+  title: string;
+  description: string;
+};
+
 function App() {
-  const { todos, handleMarkAsDone, setTodos } = useTodosState();
   const { formState, handleInputChange, setFormState } = useForm({
     title: "",
     description: "",
   });
 
   const handleAddTodo = () => {
-    setTodos((prevTodos) => [...prevTodos, { ...formState, complete: false }]);
     setFormState({ title: "", description: "" });
   };
-
-  const pendingTodos = todos.filter((todo) => !todo.complete);
-  const completedTodos = todos.filter((todo) => todo.complete);
 
   return (
     <div className="App">
@@ -38,42 +38,11 @@ function App() {
       <button className="add-button" onClick={handleAddTodo}>
         Add Todo
       </button>
-      <div className="todo-list" role="list" aria-label="Pending Todo">
-        <h2 className="todo-heading">Pending Todo</h2>
-        {pendingTodos.map((todo, index) => (
-          <div className="todo-item" key={index}>
-            <p className="todo-title">{todo.title}</p>
-            <p className="todo-description">{todo.description}</p>
-            <button
-              className="mark-done-button"
-              onClick={() => handleMarkAsDone(index)}
-            >
-              Mark as Done
-            </button>
-          </div>
-        ))}
-      </div>
-      <div className="todo-list" role="list" aria-label="Completed Todo">
-        <h2 className="todo-heading">Completed Todo</h2>
-        {completedTodos.map((todo, index) => (
-          <div className="todo-item" key={index}>
-            <p className="todo-title">{todo.title}</p>
-            <p className="todo-description">{todo.description}</p>
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
 
 export default App;
-
-type FormFields = {
-  title: string;
-  description: string;
-};
-
-type Todo = FormFields & { complete: boolean };
 
 function useForm(initialState: FormFields) {
   const [formState, setFormState] = useState<FormFields>(initialState);
@@ -86,17 +55,4 @@ function useForm(initialState: FormFields) {
   };
 
   return { formState, handleInputChange, setFormState };
-}
-
-function useTodosState(initialState: Todo[] = []) {
-  const [todos, setTodos] = useState<Todo[]>(initialState);
-
-  const handleMarkAsDone = (index: number) => {
-    const newTodos = [...todos];
-    // Here we toggle the complete state of the todo
-    newTodos[index] = { ...newTodos[index], complete: true };
-    setTodos(newTodos);
-  };
-
-  return { todos, handleMarkAsDone, setTodos };
 }
